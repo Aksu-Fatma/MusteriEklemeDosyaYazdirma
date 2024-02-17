93ec208a36bdae4fdd4781f75be9dc3fdfb941a5
Aksu fatma <fatmaksu46@gmail.com>
2024-02-17 21:27:52 +0300

    Create Notes.cs

---
+++ AksuProje/Notes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AksuProje
+{
+    public class Notes
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime CeraetedAt { get; set; }
+        public required string Text { get; set; }
+
+       
+    }
+}
