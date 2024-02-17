f09ffefd589b5de6145d26f56c535da2763c6fab
Aksu fatma <fatmaksu46@gmail.com>
2024-02-17 21:27:50 +0300

    Create IUserAction.cs

---
+++ AksuProje/IUserAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AksuProje
+{
+    public interface IUserAction
+    {
+        void AddUser(Users users);
+        public void UpdateUsersFile();
+        List<Users> GetUsersList();
+        void DeleteUser(string PhoneNumber);
+        void GetByUserFilter(string filter);
+        Users AuthenticateUser(string Email, string Password);
+        bool AuthenticateUser(Users users, string Password);
+
+    }
+}
