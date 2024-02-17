e068b3849dc32fb6bf4a87c5e21d124fd1dc7b66
Aksu fatma <fatmaksu46@gmail.com>
2024-02-17 21:27:44 +0300

    Create INoteAction.cs

---
+++ AksuProje/INoteAction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AksuProje
+{
+    public interface INoteAction
+    {
+        void AddNote(Users user, string noteContent, string noteDate);
+        void GetNoteList(Users user);
+    }
+}
